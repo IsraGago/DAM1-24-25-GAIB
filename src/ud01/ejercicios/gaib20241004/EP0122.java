f5a1bd3f17c7726aa22766b2d499c74126dc8992
Israel <igagoacun@ieschandomonte.edu.es>
2024-10-08 13:47:16 +0200

    Añado detalles al ejercicios de conversion de grados

--- src/ud01/ejercicios/gaib20241004/EP0122.java
+++ src/ud01/ejercicios/gaib20241004/EP0122.java
@@ -1,18 +1,20 @@
 package ud01.ejercicios.gaib20241004;
 
 import java.util.Scanner;
+// import java.util.Locale;
 
 public class EP0122 {
     public static void main(String[] args) {
         // SI VAS A PONER UN GRADO CON DECIMALES TIENES QUE HACERLO CON ESTE FORMATO 33,8 NO 33.8
         double farenheit,celsius;
         Scanner sc = new Scanner(System.in);
+        // sc.useLocale(Locale.US);
         System.out.println("CONVERTIR DE GRADOS FARENHEIT A CELSIUS");
         System.out.print("Ingrese los grados Fahrenheit: ");
         farenheit = sc.nextDouble();
         sc.close();
         celsius = 5/9.0 * (farenheit-32);
-        System.out.println();
-        System.out.println(farenheit+" grados Farenheit son "+celsius+" grados Celsius.");
+        // System.out.println(farenheit+" grados Farenheit son "+celsius+" grados Celsius.");
+        System.out.printf(farenheit+" grados Farenheit son %.2f grados Celsius",celsius);
     }
 }
