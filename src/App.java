6e86be179ad4cd08275ae301c8cfda08893bd2a0
Israel <igagoacun@ieschandomonte.edu.es>
2024-10-08 12:43:01 +0200

    actualización de App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! My name is GAIB");
     }
 }
