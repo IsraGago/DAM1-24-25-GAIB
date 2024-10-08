96682bf88163da1c7d718d24f1a9e576354d8bdb
Israel <igagoacun@ieschandomonte.edu.es>
2024-10-08 13:46:46 +0200

    Ejercicios 117 y 120

---
+++ src/ud01/ejercicios/EP0117.java
@@ -0,0 +1,23 @@
+package ud01.ejercicios;
+
+import java.util.Scanner;
+
+public class EP0117 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int hormigas,aranas,cochinillas,total;
+        System.out.println("CONTADOR DE PATAS DE INSECTOS");
+        System.out.print("Inserte el número hormigas capturadas: ");
+        hormigas = sc.nextInt()*6;
+        System.out.print("Inserte el número arañas capturadas: ");
+        aranas = sc.nextInt()*8;
+        System.out.print("Inserte el número cochinillas capturadas: ");
+        cochinillas = sc.nextInt()*14;
+
+        total = hormigas+aranas+cochinillas;
+
+        System.out.println("El número total de patas de esta joranada es: "+total);
+        
+        sc.close();
+    }
+}
