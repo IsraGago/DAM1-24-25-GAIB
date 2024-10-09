bcb0579ddee048a968b9c1cf2b77662072e167d5
IsraelGago <igagoacun@ieschandomonte.edu.es>
2024-10-09 19:50:28 +0200

    Actualización del gitignore

--- bin/ud01/parejas/P01Python.py
+++
@@ -1,2 +0,0 @@
-print ("Gago Acuña Israel Benjamín (GAIB - 3)")
-print ("Compañero de prácticas: Hugo Gómez")
--- bin/ud1/parejas/P01C.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf( "Gago Acuña Israel Benjamín (GAIB - 3) \n");
-    printf( "Compañero de prácticas: Hugo Gómez" );
-
-    return 0;
-}
