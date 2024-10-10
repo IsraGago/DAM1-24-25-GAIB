ffab1c04de2d997fd1b14213bb661f85298072ac
Israel <igagoacun@ieschandomonte.edu.es>
2024-10-10 10:59:32 +0200

    Edición de formato en el programa de unidades de medida

--- src/ud1/ejercicios/parejas/UnidadesDeMedidas.java
+++ src/ud1/ejercicios/parejas/UnidadesDeMedidas.java
@@ -10,19 +10,14 @@ public class UnidadesDeMedidas {
     public static void main(String[] args) {
          
         Scanner sc = new Scanner(System.in);
-
         double hectareas, camposFutbol, canchasBaloncesto, pistasTenis, parqueRetiro;
 
         System.out.print("Introduzca el numero de hectareas: ");
         hectareas = sc.nextDouble();
         sc.close();
-
         camposFutbol = (hectareas * 10000) / (105.0 * 70.0);
-
         canchasBaloncesto = (hectareas * 10000) / (28.0 * 15.0);
-
         pistasTenis = (hectareas * 10000) / (23.77 * 10.97);
-
         parqueRetiro = (hectareas * 10000) / (125.0 * 10000.0);
 
         System.out.printf("%.2f hectareas son: %.2f campos de futbol %n", hectareas, camposFutbol );
