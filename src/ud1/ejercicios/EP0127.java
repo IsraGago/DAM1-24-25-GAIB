0813a6162b0b074e8d076c5a031e4c2dd2a33f28
IsraelGago <igagoacun@ieschandomonte.edu.es>
2024-10-15 16:41:43 +0200

    ejercicio de intercambiar los valores de dos variables

---
+++ src/ud1/ejercicios/EP0127.java
@@ -0,0 +1,13 @@
+package ud1.ejercicios;
+
+public class EP0127 {
+    public static void main(String[] args) {
+        int a,b,temporal;
+        a = 1;
+        b = 2;
+
+        temporal = a;
+        a = b;
+        b = temporal;
+    }
+}
