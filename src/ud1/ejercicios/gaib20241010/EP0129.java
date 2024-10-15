18b3ea32af8f3c25fb486378217cdd142a464c6d
Israel <igagoacun@ieschandomonte.edu.es>
2024-10-15 13:48:36 +0200

    Corrección del ejercicio de los multiplos de 7

--- src/ud1/ejercicios/gaib20241010/EP0129.java
+++ src/ud1/ejercicios/gaib20241010/EP0129.java
@@ -1,16 +1,22 @@
 package ud1.ejercicios.gaib20241010;
+
 import java.util.Scanner;
 
 public class EP0129 {
     public static void main(String[] args) {
-        int numero = 2;
+        int numero;
+        String mensaje;
         final int NUMERO_MULTIPLOS = 7;
         Scanner sc = new Scanner(System.in);
-        // System.out.print("Inserte el número: ");
-        // numero = sc.nextInt();
+        System.out.print("Inserte el número: ");
+        numero = sc.nextInt();
         sc.close();
 
-        System.err.println(NUMERO_MULTIPLOS - (numero % NUMERO_MULTIPLOS));
+        mensaje = numero % NUMERO_MULTIPLOS == 0 ? "El número " + numero + " ya es múltiplo de 7"
+                : "Para el siguiente múltiplo de " + NUMERO_MULTIPLOS+" ("+(numero+(NUMERO_MULTIPLOS - numero % NUMERO_MULTIPLOS))+")"+ " hay que sumar "
+                        + (NUMERO_MULTIPLOS - numero % NUMERO_MULTIPLOS);
+
+        System.out.println(mensaje);
 
     }
 }
