682080656c62ebec458abc0626b424f4dcdd1deb
IsraelGago <igagoacun@ieschandomonte.edu.es>
2024-10-16 00:45:54 +0200

    ampliación del ejercicio del cajero

--- src/ud1/ejercicios/gaib20241015/CajeroAutomatico.java
+++ src/ud1/ejercicios/gaib20241015/CajeroAutomatico.java
@@ -6,7 +6,7 @@ public class CajeroAutomatico {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double importe, resto;
-        int billetes20, billetes50, billetes5, billetes10;
+        int billetes20, billetes50, billetes5, billetes10,monedas1,monedas2;
         System.out.print("Importe en efectivo que desea retirar (€): ");
         importe = sc.nextInt();
 
@@ -22,12 +22,20 @@ public class CajeroAutomatico {
         billetes5 = (int) resto / 5;
         resto = resto % 5;
 
+        monedas2 = (int) resto / 2;
+        resto = resto % 2;
+
+        monedas1 = (int) resto / 1;
+        resto = resto % 1;
+
         System.out.println(resto);
         System.out.println("Se retirarán " + importe + " euros");
         System.out.println(billetes50 + " billetes de 50 euros: ");
         System.out.println(billetes20 + " billetes de 20 euros: ");
         System.out.println(billetes10 + " billetes de 10 euros: ");
         System.out.println(billetes5 + " billetes de 5 euros: ");
+        System.out.println(monedas2 + " monedas de 2 euros: ");
+        System.out.println(monedas1 + " billetes de 1 euro: ");
         System.out.println(resto > 0 ? resto + " euros no se retirarán por no haber billetes tan pequeños" : "");
         sc.close();
     }
