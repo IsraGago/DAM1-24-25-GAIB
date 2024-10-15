2636b9c7a683096ba0d405c80c668d9cd5a4da67
Israel <igagoacun@ieschandomonte.edu.es>
2024-10-15 13:53:55 +0200

    Mejora de la legibilidad del código en el ejercicio de los múltiplos de 7

--- src/ud1/ejercicios/gaib20241010/EP0129.java
+++ src/ud1/ejercicios/gaib20241010/EP0129.java
@@ -4,17 +4,19 @@ import java.util.Scanner;
 
 public class EP0129 {
     public static void main(String[] args) {
-        int numero;
+        int numero, diferencia;
         String mensaje;
         final int NUMERO_MULTIPLOS = 7;
         Scanner sc = new Scanner(System.in);
         System.out.print("Inserte el número: ");
         numero = sc.nextInt();
+        diferencia = NUMERO_MULTIPLOS - numero % NUMERO_MULTIPLOS;
         sc.close();
 
         mensaje = numero % NUMERO_MULTIPLOS == 0 ? "El número " + numero + " ya es múltiplo de 7"
-                : "Para el siguiente múltiplo de " + NUMERO_MULTIPLOS+" ("+(numero+(NUMERO_MULTIPLOS - numero % NUMERO_MULTIPLOS))+")"+ " hay que sumar "
-                        + (NUMERO_MULTIPLOS - numero % NUMERO_MULTIPLOS);
+                : "Para el siguiente múltiplo de " + NUMERO_MULTIPLOS + " (" + (numero + diferencia) + ")"
+                        + " hay que sumar "
+                        + diferencia;
 
         System.out.println(mensaje);
 
