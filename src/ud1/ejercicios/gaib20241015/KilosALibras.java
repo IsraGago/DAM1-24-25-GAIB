d3af35bf9864b94ccff445ba6b090adacab36768
Israel <igagoacun@ieschandomonte.edu.es>
2024-10-15 14:01:44 +0200

    Ejercicio de pasar de kilos a libras

--- src/ud1/ejercicios/gaib20241015/KilosALibras.java
+++ src/ud1/ejercicios/gaib20241015/KilosALibras.java
@@ -1,8 +1,19 @@
 package ud1.ejercicios.gaib20241015;
+import java.util.Scanner;
 
 public class KilosALibras {
     public static void main(String[] args) {
         /* Diseña un programa que lea una medida de peso en kilogramos y la devuelva en libras. Una
         libra equivale a 0.453592 kilogramos.*/
+        double kg,lb;
+        final double KG_LIBRAS = 0.453592;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Inserte la cantidad de kilogramos que desea pasar a libras: ");
+        kg = sc.nextDouble();
+        sc.close();
+        lb = kg / KG_LIBRAS;
+
+        System.out.printf("%.2f kg son %.2f libras.",kg,lb);        
     }
 }
