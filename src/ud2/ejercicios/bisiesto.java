5fa36b8a7245db31cfb3d7a2612f71bd35fe6d9a
Israel <igagoacun@ieschandomonte.edu.es>
2024-10-18 13:30:24 +0200

    Mejora de la legibilidad del código del ejercicio de calcular si un año es bisiesto

--- src/ud2/ejercicios/bisiesto.java
+++ src/ud2/ejercicios/bisiesto.java
@@ -5,12 +5,17 @@ import java.util.Scanner;
 public class Bisiesto {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int anho;
+        short anho;
+        boolean esBisiensto;
         String mensaje = "";
+
         System.out.print("Inserte el número de un año: ");
-        anho = sc.nextInt();
+        anho = sc.nextShort();
         sc.close();
-        if ((anho % 4 == 0 && anho % 100 != 0) || anho % 400 == 0) {
+
+        esBisiensto = (anho % 4 == 0 && anho % 100 != 0) || anho % 400 == 0;
+
+        if (esBisiensto) {
             mensaje = "El año " + anho + " es bisiesto ";
         } else {
             mensaje = "El año " + anho + " no es bisiesto ";
