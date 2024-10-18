d55b0ce6a1dde3e7ae8215982a2fa4352e92c57e
Israel <igagoacun@ieschandomonte.edu.es>
2024-10-18 14:24:51 +0200

    Ejercicio de contar las cifras de un número entero

--- src/ud2/ejercicios/E0209.java
+++ src/ud2/ejercicios/E0209.java
@@ -1,7 +1,36 @@
 package ud2.ejercicios;
 
+import java.util.Scanner;
+
 public class E0209 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int numero, cifras = 0, numeroAbsoluto;
+        System.out.print("Introduzca un número entre 0 y 100.000: ");
+        numero = sc.nextInt();
+        sc.close();
+
+        numeroAbsoluto = Math.abs(numero);
+
+        if (numeroAbsoluto > 0 && numeroAbsoluto < 100000) {
+            cifras = 1;
+            if (numeroAbsoluto > 10) {
+                cifras = 2;
+                if (numeroAbsoluto > 100) {
+                    cifras = 3;
+                    if ( numeroAbsoluto > 1000){
+                        cifras = 4;
+                        if (numeroAbsoluto > 10000) {
+                            cifras = 5;
+                        }
+                    }
+                }
+            }
+        } else {
+            System.out.println("El número tiene que ser mayor que 0 y menor que 100.000");
+        }
+        if (cifras > 0){
+            System.out.println("El número "+numero+" tiene "+cifras+" cifras");
+        }
     }
 }
