97d34cd0a69ee8d30cfd2918b763b3ee46cd941d
IsraelGago <igagoacun@ieschandomonte.edu.es>
2024-10-23 21:41:16 +0200

    Calcular el valor absoluto de un número

--- src/ud2/ejercicioscondicionales/EP0216.java
+++ src/ud2/ejercicioscondicionales/EP0216.java
@@ -1,7 +1,14 @@
 package ud2.ejercicioscondicionales;
 
+import java.util.Scanner;
+
 public class EP0216 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Itroduzca un número para calcular su valor absoluto: ");
+        int numero = sc.nextInt();
+        int absoluto = numero > 0 ? numero : numero-numero-numero;
+        System.out.println("El valor absoluto de "+numero+" es "+absoluto);
+        sc.close();
     }
 }
