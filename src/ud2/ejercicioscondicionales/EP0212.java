2ce65b6bfeb65917ea713d7df3c84e2f3f532e4e
IsraelGago <igagoacun@ieschandomonte.edu.es>
2024-10-23 17:22:29 +0200

    Calcular la letra de un DNI

---
+++ src/ud2/ejercicioscondicionales/EP0212.java
@@ -0,0 +1,19 @@
+package ud2.ejercicioscondicionales;
+
+import java.util.Scanner;
+
+public class EP0212 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduzca un número entero de 8 dígitos: ");
+        int numero = sc.nextInt();
+        char letra;
+        String letras ="TRWAGMYFPDXBNJZSQVHLCKE";
+        sc.close();
+
+        letra = letras.charAt(numero % 23);
+        String dni = numero+""+letra;
+        System.out.println("Su DNI completo es: "+dni);
+
+    }
+}
