f2b29535027632d8fe654594f50f61a9e028e161
Israel <igagoacun@ieschandomonte.edu.es>
2024-10-29 13:57:51 +0100

    Ejemplos de bucles while

---
+++ src/ud2/ejemplos/Ejemplo1While.java
@@ -0,0 +1,20 @@
+package ud2.ejemplos;
+
+import java.util.*;
+
+public class Ejemplo1While {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num; // variable que contiene el número introducido
+        int suma = 0; // variable donde acumularemos la suma de los números
+        System.out.print("Introduzca un número ( < 0 para finalizar): ");
+        num = sc.nextInt(); // lectura del primer número
+        while (num >= 0) { // inicio del bucle while
+            suma = suma + num; // se suma el número introducido
+            System.out.print("Introduzca un número ( < 0 para finalizar): ");
+            num = sc.nextInt(); // lectura del siguiente número
+        } // fin del bucle while
+        sc.close();
+        System.out.println("La suma es: " + suma); // se muestra la suma
+    }
+}
