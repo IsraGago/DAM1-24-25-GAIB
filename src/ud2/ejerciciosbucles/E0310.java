cce765dff87d4f98be72132582ef7db1c16d2158
Israel <igagoacun@ieschandomonte.edu.es>
2024-10-30 10:58:32 +0100

    Suma de los primeros 10 números impares

---
+++ src/ud2/ejerciciosbucles/E0310.java
@@ -0,0 +1,17 @@
+package ud2.ejerciciosbucles;
+
+public class E0310 {
+    public static void main(String[] args) {
+        int suma = 0;
+        int contadorImpares = 0;
+        final int MAX = 10;
+        for (int i = 0; contadorImpares < MAX; i++) {
+            if (i % 2 == 0) {
+                contadorImpares++;
+                suma += i;
+            }
+        }
+        System.out.printf("La suma de los %d primeros números impares es: %d",MAX,suma);
+        
+    }
+}
