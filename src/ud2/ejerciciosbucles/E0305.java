2c852b283e824f5ce6b774934bb172ce0b6270d2
Israel <igagoacun@ieschandomonte.edu.es>
2024-10-30 10:42:02 +0100

    Juego de sumar números aleatorios hasta que falles N veces

--- src/ud2/ejerciciosbucles/E0305.java
+++ src/ud2/ejerciciosbucles/E0305.java
@@ -5,27 +5,32 @@ import java.util.Random;
 
 public class E0305 {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       Random random = new Random();
-       int a,b,resultado,respuesta,puntos = 0;
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        int a, b, resultado, respuesta, puntos = 0, numFallos = 0;
+        final int MAX_FALLOS = 3;
 
-       System.out.println("JUEGO DE SUMAR NÚMEROS");
-       System.out.println("RESPONDE LAS OPERACIONES ARITMETICAS HASTA QUE FALLES");
-       do {
-           a = random.nextInt(1, 100);
-           b = random.nextInt(1, 100);
-           resultado = a + b;
-           System.out.printf("%d + %d = ", a, b);
-           respuesta = sc.nextInt();
-           if (respuesta == resultado) {
-            puntos++;
-            System.out.println("Correcto!!!");
-            System.out.println("----------------------------");
-           }
-       } while (respuesta == resultado);
-       System.out.println("Has perdido...");
-       System.out.println("Puntuación: "+puntos);
-       sc.close();
+        System.out.println("JUEGO DE SUMAR NÚMEROS");
+        System.out.println("RESPONDE LAS OPERACIONES ARITMETICAS HASTA QUE FALLES");
+        do {
+            a = random.nextInt(1, 100);
+            b = random.nextInt(1, 100);
+            resultado = a + b;
+            System.out.printf("%d + %d = ", a, b);
+            respuesta = sc.nextInt();
+            if (respuesta == resultado) {
+                puntos++;
+                System.out.println("Correcto!!!");
+                System.out.println("----------------------------");
+            }else{
+                numFallos++;
+                System.out.printf("Has fallado... te quedan %d intentos%n",MAX_FALLOS-numFallos);
+                System.out.println("----------------------------");
+            }
+        } while (numFallos != MAX_FALLOS);
+        System.out.println("Has perdido...");
+        System.out.println("Puntuación: " + puntos);
+        sc.close();
 
     }
 }
