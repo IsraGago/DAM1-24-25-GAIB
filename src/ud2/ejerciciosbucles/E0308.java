d96adad52163732a7b33a94951e322060ab1f102
Israel <igagoacun@ieschandomonte.edu.es>
2024-10-30 10:54:34 +0100

    media de N números

---
+++ src/ud2/ejerciciosbucles/E0308.java
@@ -0,0 +1,21 @@
+package ud2.ejerciciosbucles;
+
+import java.util.Scanner;
+
+public class E0308 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double suma = 0;
+        final int MAX = 10;
+
+        for (int i = 0; i <= MAX; i++) {
+            System.out.printf("Introduzca un número para hacer la media (%d/%d): ",i,MAX);
+            suma += sc.nextDouble();
+        }
+
+        double media =  suma / MAX;
+        System.out.printf("La media de los %d números introducidos es: %.2f",MAX,media); 
+
+        sc.close();
+    }
+}
