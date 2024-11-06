bfb506bc5c356e4ff1d4ff438869dc3cac8ac226
IsraelGago <igagoacun@ieschandomonte.edu.es>
2024-11-06 16:11:12 +0100

    funciones : numCifras, notaEnTexto y diaSemana

--- src/ud2/Util.java
+++ src/ud2/Util.java
@@ -30,5 +30,39 @@ public class Util {
         return true;
         
     }
+    static int numCifras (int numero){
+        numero = Math.abs(numero);
+        int cifras = 0;
+        while (numero > 0) {
+            numero = numero / 10;
+            cifras++;
+        }
+        return cifras;
+    }
+    static String notaEnTexto(int nota){
+        switch (nota) {
+            case 0,1,2,3,4: return "insuficiente";
+            case 5: return "suficiente";
+            case 6: return "bien";
+            case 7,8: return "notable";
+            case 9,10: return "sobresaliente";
+            default:
+                return "Nota inválida (0-10)"; 
+        }
+    }
+
+    static String diaSemana(int numDia){
+        switch (numDia) {
+            case 1: return "Lunes";
+            case 2: return "Martes";
+            case 3: return "Miércoles";
+            case 4: return "Jueves";
+            case 5: return "Viernes";
+            case 6: return "Sábado";
+            case 7: return "Domingo";
+            default:
+                return "dia inválido (1-7)"; 
+        }
+    }
 
 }
