1a69d369837ec012a78d7bd7d390f732cb438d92
IsraGago <igagoacun@ieschandomonte.edu.es>
2024-11-07 12:46:47 +0100

    Bordear String

--- src/ud2/funciones/RecuadroString.java
+++ src/ud2/funciones/RecuadroString.java
@@ -4,26 +4,29 @@ import java.util.Scanner;
 
 public class RecuadroString {
 
-    public static void cajaTexto(String str){
-        imprimirLinea(str);
-        System.out.println("# " + str + " #");
-        imprimirLinea(str);
+    public static void cajaTexto(String str,char caracter){
+        imprimirLinea(str,caracter);
+        System.out.println(caracter+" " + str + " "+caracter);
+        imprimirLinea(str,caracter);
 
     }
-    public static void imprimirLinea(String str){
+    public static void imprimirLinea(String str,char caracter){
         int n = str.length();
         for (int i = 1; i <= n + 4; i++){
-             System.out.print("#"); 
+             System.out.print(caracter);
         }
         System.out.println();
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String cadena;
+        char caracter;
         System.out.print("Introduce cadena de texto: ");
         cadena = sc.nextLine();
+        System.out.print("Introduzca el caracter para bordear: ");
+        caracter = sc.nextLine().charAt(0);
         sc.close();
-        cajaTexto(cadena);
+        cajaTexto(cadena,caracter);
     }
 
 }
