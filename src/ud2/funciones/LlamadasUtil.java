6679fce0ab4c1af052de12d718fae35651f755d3
IsraGago <igagoacun@ieschandomonte.edu.es>
2024-11-08 14:01:10 +0100

    pruebas de llamadas

--- src/ud2/funciones/LlamadasUtil.java
+++ src/ud2/funciones/LlamadasUtil.java
@@ -5,8 +5,7 @@ import ud2.Util;
 
 public class LlamadasUtil {
     public static void main(String[] args) {
-        Util.esPar(2);
-        boolean esHoraCorrecta = Util.esHoraCorrecta(59, 59, 24);
-        System.out.println(esHoraCorrecta);
+        Util.imrpimirTrianguloRectangulo(5);
+        Util.imrpimirTrianguloRectanguloInverso(5);
     }
 }
