43a3673934c83e7e6e811a3fd4e7ca180bdd8d6b
IsraGago <igagoacun@ieschandomonte.edu.es>
2024-11-08 12:56:45 +0100

    correción de formato

--- src/ud2/funciones/BordearString.java
+++ src/ud2/funciones/BordearString.java
@@ -4,29 +4,33 @@ import java.util.Scanner;
 
 public class BordearString {
 
-    public static void cajaTexto(String str,char caracter){
-        imprimirLinea(str,caracter);
-        System.out.println(caracter+" " + str + " "+caracter);
-        imprimirLinea(str,caracter);
+    public static void cajaTexto(String str, char caracter) {
+        imprimirLinea(str, caracter);
+        System.out.println(caracter + " " + str + " " + caracter);
+        imprimirLinea(str, caracter);
 
     }
-    public static void imprimirLinea(String str,char caracter){
+
+    public static void imprimirLinea(String str, char caracter) {
         int n = str.length();
-        for (int i = 1; i <= n + 4; i++){
-             System.out.print(caracter);
+        for (int i = 1; i <= n + 4; i++) {
+            System.out.print(caracter);
         }
         System.out.println();
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String cadena;
         char caracter;
-        System.out.print("Introduce cadena de texto: ");
+
+        System.out.print("Introduzca cadena de texto: ");
         cadena = sc.nextLine();
         System.out.print("Introduzca el caracter para bordear: ");
         caracter = sc.nextLine().charAt(0);
+        
         sc.close();
-        cajaTexto(cadena,caracter);
+        cajaTexto(cadena, caracter);
     }
 
 }
