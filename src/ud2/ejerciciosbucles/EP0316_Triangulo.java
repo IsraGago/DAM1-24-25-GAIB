c61ac58ec871ffbae21b962553e1477e5b43ee46
IsraelGago <igagoacun@ieschandomonte.edu.es>
2024-11-14 15:48:11 +0100

    dibujar un cuadrado equilatero de base y altura n

--- src/ud2/ejerciciosbucles/EP0316_Triangulo.java
+++ src/ud2/ejerciciosbucles/EP0316_Triangulo.java
@@ -5,22 +5,20 @@ import java.util.Scanner;
 public class EP0316_Triangulo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Introduzca un número entero: ");
-        int numero = sc.nextInt();
+        System.out.print("Introduce el valor de n: ");
+        int n = sc.nextInt();
+        sc.close();
 
-        for (int i = 0; i < numero; i++) {
-            
-            for (int j = 0; j <= i; j++) {
-                if (j < numero / 2) {
-                    for (int j2 = numero/2; j2 > j; j2--) {
-                        System.out.print(" ");
-                    }
-                }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= i; k++) {
                 System.out.print("* ");
             }
+
             System.out.println();
         }
-
-        sc.close();
     }
 }
