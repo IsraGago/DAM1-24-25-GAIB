a392fba09d0905826d4a570ad734dd636b21b88c
IsraelGago <igagoacun@ieschandomonte.edu.es>
2024-11-14 19:28:30 +0100

    correción

--- src/ud2/ejerciciosbucles/E0308.java
+++ src/ud2/ejerciciosbucles/E0308.java
@@ -8,13 +8,13 @@ public class E0308 {
         double suma = 0;
         final int MAX = 10;
 
-        for (int i = 0; i <= MAX; i++) {
+        for (int i = 1; i <= MAX; i++) {
             System.out.printf("Introduzca un número para hacer la media (%d/%d): ",i,MAX);
             suma += sc.nextDouble();
         }
 
         double media =  suma / MAX;
-        System.out.printf("La media de los %d números introducidos es: %.2f",MAX,media); 
+        System.out.printf("La media de los %d números introducidos es: %.2f",MAX,media);
 
         sc.close();
     }
