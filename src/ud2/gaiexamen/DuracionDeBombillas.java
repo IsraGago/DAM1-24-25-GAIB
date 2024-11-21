d169d3e143f33860e79324a73fbe523afc2916cd
IsraGago <igagoacun@ieschandomonte.edu.es>
2024-11-21 12:55:33 +0100

    correción del ejercicio

--- src/ud2/gaiexamen/DuracionDeBombillas.java
+++ src/ud2/gaiexamen/DuracionDeBombillas.java
@@ -11,13 +11,14 @@ public class DuracionDeBombillas {
             return "ERROR";
         }
         int cantidadEncendidos = maxHoras / horasEncendidaPorUso;
+        // int horasMaxEstiamdasUso = maxEncendidos * horasEncendidaPorUso; otra posible solución
 
         if (cantidadEncendidos == maxEncendidos) {
             cadena = "ENCENDIDOS + HORAS";
         } else if (cantidadEncendidos < maxEncendidos) {
             cadena = "HORAS";
-        } else if (cantidadEncendidos > maxEncendidos) {
-            cadena = "ENCENDIDOS";
+        } else if (cantidadEncendidos > maxEncendidos) { // esta última comprobación sobra, con el else llega y es más eficiente,
+            cadena = "ENCENDIDOS";                       // porque no van a haber más casos
         }
         return cadena;
     }
@@ -26,9 +27,15 @@ public class DuracionDeBombillas {
     public void causaFinBombillaTest() {
         assertEquals("ERROR", causaFinBombilla(100, 100, 100));
         assertEquals("ERROR", causaFinBombilla(1000, -10, 10));
+        assertEquals("ERROR", causaFinBombilla(0, 10, 10));
         assertEquals("ERROR", causaFinBombilla(1000, 10, 0));
+        assertEquals("ENCENDIDOS", causaFinBombilla(500, 400, 1));
+        assertEquals("HORAS", causaFinBombilla(1000, 10000, 1));
         assertEquals("HORAS", causaFinBombilla(1000, 200, 10));
+        assertEquals("HORAS", causaFinBombilla(2000, 700, 3));
+        assertEquals("ENCENDIDOS", causaFinBombilla(2000, 600, 3));
         assertEquals("ENCENDIDOS", causaFinBombilla(1000, 100, 1));
+        assertEquals("ENCENDIDOS", causaFinBombilla(1000, 100, 9));
         assertEquals("ENCENDIDOS + HORAS", causaFinBombilla(1000, 100, 10));
     }
 
