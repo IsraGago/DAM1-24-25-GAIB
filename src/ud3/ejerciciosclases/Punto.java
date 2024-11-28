f8318278372e36f24cbe3ea8610a42e48cd5d50a
IsraelGago <igagoacun@ieschandomonte.edu.es>
2024-11-28 21:51:50 +0100

    clase punto

--- src/ud3/ejerciciosclases/Punto.java
+++ src/ud3/ejerciciosclases/Punto.java
@@ -1,13 +1,29 @@
 package ud3.ejerciciosclases;
 
 public class Punto {
-    private double x,y;
-    public Punto (double x , double y){
+    private double x, y;
+
+    public Punto(double x, double y) {
         this.x = x;
         this.y = y;
     }
-    public void desplazaX(double dx){
-        
-    }
 
+    public void desplazaX(double dx) {
+        x += dx;
+    }
+    public void desplazaY(double dy) {
+        y += dy;
+    }
+    public void desplaza(double dx, double dy){
+        // desplazaX(dx);
+        // desplazaY(dy);
+        x += dx;
+        y += dy;
+    }
+    public double distanciaEuclidea(Punto otro){
+        return Math.sqrt(x - Math.pow(otro.x, 2) + Math.pow((y-otro.y), 2));
+    }
+    public void muestra(){
+        System.out.printf("(%.2f, %.2f)%n",x,y);
+    }
 }
