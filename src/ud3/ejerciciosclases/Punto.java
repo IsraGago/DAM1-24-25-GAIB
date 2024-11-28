6c58f35227e76c184c04c197a58215a7836dd023
IsraGago <igagoacun@ieschandomonte.edu.es>
2024-11-28 13:48:30 +0100

    clase punto

---
+++ src/ud3/ejerciciosclases/Punto.java
@@ -0,0 +1,13 @@
+package ud3.ejerciciosclases;
+
+public class Punto {
+    private double x,y;
+    public Punto (double x , double y){
+        this.x = x;
+        this.y = y;
+    }
+    public void desplazaX(double dx){
+        
+    }
+
+}
