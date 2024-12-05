61a2d220a4c10dfd87647de4ced302a9ddc7eef0
IsraGago <igagoacun@ieschandomonte.edu.es>
2024-12-05 12:51:52 +0100

    Ejemplo clases envolventes

---
+++ src/ud3/ejemplos/Envolventes.java
@@ -0,0 +1,28 @@
+package ud3.ejemplos;
+
+public class Envolventes {
+    public static void main(String[] args) {
+        int k = 23;
+        // Integer numero = new Integer(k); // obsoleto
+        Integer entero = k; // se crea el objeto y se le asigna el valor
+        
+        String cadena = "4.21";
+        Float objetoFloat = Float.valueOf(cadena);
+
+        // float floatPrimitivo = objetoFloat.floatValue(); // obsoleto
+        float floatPrimitivo = objetoFloat;
+
+        String s1 ="25",s2 = "92.2";
+        int n = Integer.parseInt(s1);
+        double d = Double.parseDouble(s2);
+
+        int decimalABinario = Integer.parseInt("101",2);
+
+        System.out.println(entero);
+        System.out.println(floatPrimitivo);
+        System.out.println(n);
+        System.out.println(d);
+        System.out.println(decimalABinario);
+
+    }
+}
