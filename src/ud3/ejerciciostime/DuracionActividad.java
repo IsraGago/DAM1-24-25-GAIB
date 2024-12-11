5e98b0ef38c91fd77e98f68b0f9505f6c35059b1
IsraGago <igagoacun@ieschandomonte.edu.es>
2024-12-11 11:27:56 +0100

    Duracion actividad

--- src/ud3/ejerciciostime/DuracionActividad.java
+++ src/ud3/ejerciciostime/DuracionActividad.java
@@ -1,5 +1,26 @@
 package ud3.ejerciciostime;
 
-public class DuracionActividad {
+import java.time.Duration;
+import java.time.LocalTime;
 
+public class DuracionActividad {
+    public static void main(String[] args) {
+        final long MINS = 60;
+        final long HORAS = 60 * 60;
+        LocalTime horaInicio = LocalTime.of(8, 10, 0);
+        LocalTime horaFinal = LocalTime.of(14, 30, 0);
+        Duration duracion = Duration.between(horaInicio, horaFinal);
+
+        long horas = duracion.getSeconds() / HORAS;
+        long resto = duracion.getSeconds() % HORAS;
+        long minutos = resto / MINS;
+        long segundos = resto % MINS;
+        
+        
+
+        System.out.println(duracion.getSeconds()+ " Segundos totales.");
+        System.out.println(horas+" horas");
+        System.out.println(minutos+" minutos");
+        System.out.println(segundos+ " segundos");
+    }
 }
