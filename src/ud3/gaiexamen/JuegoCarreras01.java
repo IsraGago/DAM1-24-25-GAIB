b1d9baf38cd231e29b3e5cc431c5ae6b8a56f2de
IsraGago <igagoacun@ieschandomonte.edu.es>
2024-12-13 14:11:31 +0100

    Examen ud3

--- src/ud3/gaiexamen/JuegoCarreras01.java
+++ src/ud3/gaiexamen/JuegoCarreras01.java
@@ -5,12 +5,38 @@ import java.util.Random;
 public class JuegoCarreras01 {
     // AUTOR: ISRAEL BENJAMÍN GAGO ACUÑA
 
-    // public void turno(CocheCarreras coche){
-
-    // }
+    public static void jugarTurno(CocheCarreras coche) {
+        Random rd = new Random();
+        int avance = 0;
+        boolean usoTurbo = false;
+        if (coche.getAutonomiaRestante() > 0) {
+            if (coche.getTurbosRestantes() > 0) {
+                if (rd.nextInt(1, 5) == 1) { // 25% de probabilidades
+                    avance = coche.usarTurbo();
+                    usoTurbo = true;
+                } else {
+                    avance = coche.avanzar();
+                }
+            } else {
+                avance = coche.avanzar();
+            }
+        } else {
+            System.out.printf("%s para a repostar.%n", coche.getNombre());
+            coche.repostar();
+            avance = 0;
+        }
+        if (usoTurbo) {
+            System.out.printf(
+                    "%s usa un turbo y avanza %d metros. Quedan %d turbos. %n(Total recorrido: %d metros).%n",
+                    coche.getNombre(), avance, coche.getTurbosRestantes(), coche.getDistanciaRecorrida());
+        } else {
+            System.out.printf("%s avanza %d metros (Total recorrido: %d metros).%n", coche.getNombre(), avance,
+                    coche.getDistanciaRecorrida());
+        }
+    }
 
     public static void main(String[] args) {
-        Random rd = new Random();
+
         final int META_CARRERA = 500;
         int contadorTurnos = 1;
 
@@ -24,71 +50,22 @@ public class JuegoCarreras01 {
         segundo.mostrar();
 
         // EMPIEZA EL JUEGO
-        while (primero.getDistanciaRecorrida() < META_CARRERA && segundo.getDistanciaRecorrida() < META_CARRERA) {
-        System.out.printf("%dº turno %n",contadorTurnos);
-        contadorTurnos++;
-            // PRIMER COCHE
-            int avance = 0;
-            boolean usoTurbo = false;
-            if (primero.getAutonomiaRestante() > 0) {
-                if (primero.getTurbosRestantes() > 0) {
-                    if (rd.nextInt(1, 5) == 1) { // 25% de probabilidades
-                        avance = primero.usarTurbo();
-                        usoTurbo = true;
-                    } else {
-                        avance = primero.avanzar();
-                    }
-                } else {
-                    avance = primero.avanzar();
-                }
-            } else {
-                System.out.printf("%s para a repostar.%n", primero.getNombre());
-                primero.repostar();
-                avance = 0;
-            }
-            if (usoTurbo) {
-                System.out.printf(
-                        "%s usa un turbo y avanza %d metros. Quedan %d turbos. %n(Total recorrido: %d metros).%n",
-                        primero.getNombre(), avance, primero.getTurbosRestantes(), primero.getDistanciaRecorrida());
-            } else {
-                System.out.printf("%s avanza %d metros (Total recorrido: %d metros).%n", primero.getNombre(), avance,
-                        primero.getDistanciaRecorrida());
+        while (primero.getDistanciaRecorrida() <= META_CARRERA && segundo.getDistanciaRecorrida() <= META_CARRERA) {
+            System.out.printf("%dº turno %n", contadorTurnos);
+            contadorTurnos++;
+
+            jugarTurno(primero);
+            if (primero.getDistanciaRecorrida() <= META_CARRERA) { // EL SEGUNDO COCHE NO JUEGA TURNO PORQUE YA GANÓ EL PRIMERO
+                jugarTurno(segundo);
             }
 
-            // SEGUNDO COCHE
-            avance = 0;
-            usoTurbo = false;
-            if (segundo.getAutonomiaRestante() > 0) {
-                if (segundo.getTurbosRestantes() > 0) {
-                    if (rd.nextInt(1, 5) == 1) { // 25% de probabilidades
-                        avance = segundo.usarTurbo();
-                        usoTurbo = true;
-                    } else {
-                        avance = segundo.avanzar();
-                    }
-                } else {
-                    avance = segundo.avanzar();
-                }
-            } else {
-                System.out.printf("%s para a repostar", segundo.getNombre());
-                segundo.repostar();
-                avance = 0;
-            }
-            if (usoTurbo) {
-                System.out.printf(
-                        "%s usa un turbo y avanza %d metros. Quedan %d turbos. %n(Total recorrido: %d metros).%n",
-                        segundo.getNombre(), avance, segundo.getTurbosRestantes(), segundo.getDistanciaRecorrida());
-            } else {
-                System.out.printf("%s avanza %d metros (Total recorrido: %d metros).%n", segundo.getNombre(), avance,
-                        segundo.getDistanciaRecorrida());
-            }
             System.out.println("-------------------------------------------------------");
         }
 
-        if (primero.getDistanciaRecorrida() > 500) {
-            System.out.printf("%s ha cruzado la meta y es el ganador!",primero.getNombre());
+        if (primero.getDistanciaRecorrida() >= META_CARRERA) {
+            System.out.printf("%s ha cruzado la meta y es el ganador!", primero.getNombre());
         } else {
-            System.out.printf("%s ha cruzado la meta y es el ganador!",segundo.getNombre());
+            System.out.printf("%s ha cruzado la meta y es el ganador!", segundo.getNombre());
         }
     }
 
